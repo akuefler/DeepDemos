b35752ed266efba2f984f24736916604a96a7c0b
Alex Kuefler <kuefleralex@gmail.com>
2016-08-29 23:22:42 -0700

    Provisional changes. Still not working.

--- reinforcement/policy_gradients.py
+++ reinforcement/policy_gradients.py
@@ -19,6 +19,7 @@ def mlp(x, hidden_spec):
         b= tf.get_variable('b%i'%i, shape= (h,))
         layer= f(tf.nn.xw_plus_b(layer, w, b))
         h_prev = h
+        
     return layer
 
 def get_dim(space):
@@ -53,7 +54,8 @@ class Critic(object):
 class TrajBatch(object):
     def __init__(self, batch_size):
         self.batch_size = batch_size
-        self.sampled_episodes= []
+        #self.sampled_episodes= []
+        self.O, self.A, self.R, self.L = [], [], [], []
         self.curr_traj= {'obs':[],'act':[],'rew':[],'scc':[]}
     
     def update(self,o,a,r,o_,done):
@@ -68,16 +70,25 @@ class TrajBatch(object):
             act_batch = np.concatenate([a[None] for a in self.curr_traj['act']], axis= 0)
             
             cum_ret_batch = np.cumsum(self.curr_traj['rew'])[...,None]
-            ret_batch = np.ones_like(cum_ret_batch) * cum_ret_batch[-1]
+            ret_batch = cum_ret_batch - np.mean(cum_ret_batch)
             
-            self.sampled_episodes.append((obs_batch,act_batch,cum_ret_batch))
+            self.O.append(obs_batch)
+            self.A.append(act_batch)
+            self.R.append(ret_batch)
+            self.L.append(len(self.curr_traj['obs']))
+            
+            #self.sampled_episodes.append((obs_batch,act_batch,cum_ret_batch))
             self.curr_traj= {'obs':[],'act':[],'rew':[],'scc':[]}
             
     def get_batch(self):
         batch= []
-        if len(self.sampled_episodes) >= self.batch_size:
-            batch= self.sampled_episodes
-            self.sampled_episodes= []
+        if len(self.O) >= self.batch_size:
+            #batch= self.sampled_episodes
+            batch = (np.concatenate(self.O),
+                     np.concatenate(self.A),
+                     np.concatenate(self.R),
+                     np.array(self.L))
+            self.O, self.A, self.R, self.L = [],[],[],[]
         return batch
         
     
@@ -91,6 +102,7 @@ class SoftmaxNet(Policy):
         self.o_input = tf.placeholder(tf.float32, shape=(None,self.o_dim), name='o_input')
         self.a_input = tf.placeholder(tf.int32, shape=(None,), name='a_input')
         self.r_input = tf.placeholder(tf.float32,shape=(None,),name='r_input')
+        self.len_input=tf.placeholder(tf.int32,shape=(None,),name='len_input')
         
         with tf.variable_scope('encoder'):
             h = mlp(self.o_input, hidden_spec)
@@ -104,16 +116,12 @@ class SoftmaxNet(Policy):
         self.pi = tf.nn.softmax(logits)
         self.actiondist = tf.multinomial(logits, 1)
         
-        #self.obj = tf.reduce_mean(tf.reduce_sum(logits - tf.expand_dims(tf.reduce_mean(logits,1),1),1)) * self.r_input
-        #self.obj = tf.nn.sparse_softmax_cross_entropy_with_logits(logits,self.a_input) * self.r_input
-        #self.obj = tf.reduce_mean(tf.reduce_sum(logits - tf.expand_dims(tf.reduce_mean(logits,1),1),1)) * self.r_input
-        #self.obj = tf.reduce_mean((gather_nd(logits, self.a_input) - tf.reduce_mean(logits,1)) * self.r_input)
-        #self.obj = tf.reduce_mean(
-            #tf.nn.sparse_softmax_cross_entropy_with_logits(logits,self.a_input) * self.r_input
-            #)
-        #self.obj = -tf.reduce_mean(
-            #(tf.reduce_max(logits,1) - tf.reduce_mean(logits,1)) * self.r_input
-            #)
+        ## ??
+        self.obj = tf.reduce_mean(
+                -1 * tf.nn.sparse_softmax_cross_entropy_with_logits(self.logits,self.a_input)\
+                * self.r_input,
+            reduction_indices= 0
+            )
         
         self.opt = tf.train.GradientDescentOptimizer(learning_rate=.1)
         self.sess.run(tf.initialize_all_variables())
@@ -129,31 +137,13 @@ class SoftmaxNet(Policy):
         self.t_batch.update(o,a,r,o_,done)
         batch = self.t_batch.get_batch()
         if batch != []:
-            #[(obs, act, ret),...]
-            sampled_episodes = batch
-            grads_and_var= self.opt.compute_gradients(self.logits)
-            grads_and_vars2=self.opt.compute_gradients(tf.reduce_mean(self.logits))
+            #Stack of all episodes, all timesteps
+            (O,A,R,L) = batch
             
-            weight_grad_est= {var:[] for _, var in grads_and_var}            
-            for obs_batch, act_batch, ret_batch in sampled_episodes:
-                for (grad, var),(grad2,var2) in zip(grads_and_var,grads_and_vars2):
-                    gradient, gradient2, logits = self.sess.run([grad,grad2,self.logits],feed_dict={self.o_input:obs_batch,
-                                                             self.a_input:act_batch[:,0],
-                                                              #self.a_input:np.column_stack((np.arange(0,act_batch.shape[0]),
-                                                                                            #act_batch[:,0])),
-                                                              self.r_input:ret_batch[:,0]})
-                    assert not np.isnan(gradient.sum())
-                    #assert not (gradient.sum()==0.)
-                    weight_grad_est[var].append(gradient)
-                    
-            for var, m_gradients in weight_grad_est.iteritems():
-                gradient = np.mean(np.concatenate([g[...,None] for g in weight_grad_est[var]],
-                                                  axis= -1),axis= -1)
-                
-                assert not np.isnan(gradient.sum())
-                assign_op = var.assign_add(gradient)
-                self.sess.run(assign_op)
-            
-        
+            grads = tf.gradients(self.obj, tf.trainable_variables())
+            for var, grad in zip(tf.trainable_variables(),grads):
+                assign_op = var.assign_add(grad * R.mean())
+                self.sess.run(assign_op, feed_dict={self.o_input: O,
+                                                    self.a_input: A})
             
         
